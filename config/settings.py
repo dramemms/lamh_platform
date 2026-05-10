293b80ffacffb34fb5d50449dbf9a0841e64daf2
Mahamadou Dramé <dramemms@gmail.com>
2026-05-10 14:50:51 +0000

    Add Kobo assistance form URL setting

--- config/settings.py
+++ config/settings.py
@@ -56,9 +56,9 @@ CSRF_TRUSTED_ORIGINS = [
 # KOBO
 KOBO_WEBHOOK_TOKEN = "SECRET_LAMH_2026"
 
-KOBO_PAI_FORM_URL = os.environ.get(
+KOBO_ASSISTANCE_FORM_URL = os.environ.get(
     "KOBO_ASSISTANCE_FORM_URL",
-    "https://ee.kobotoolbox.org/x/4TMcZ5nH"
+    ""
 )
 
 
